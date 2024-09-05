9c81af905a184dd9c33f87cf518c475f6235e9d5
WellingtonEugenio <wellingtoneugenio@gmail.com>
2024-09-05 17:21:59 -0300

    modificando as classes de models com o publisher_id

--- src/main/java/com/bookstore/jpa/models/PublisherModel.java
+++ src/main/java/com/bookstore/jpa/models/PublisherModel.java
@@ -1,13 +1,19 @@
 package com.bookstore.jpa.models;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +27,10 @@ public class PublisherModel implements Serializable{
 	
 	@Column(nullable = false, unique = true)
 	private String name;
+	
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@OneToMany(mappedBy = "publisher", fetch = FetchType.LAZY)
+	private Set<BookModel> books = new HashSet<>();
 
 	public UUID getId() {
 		return id;
